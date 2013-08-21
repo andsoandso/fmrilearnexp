8db33934d9523ff09c84510c02c7073041dcb40c
Erik Peterson <Erik.Exists@gmail.com>
2013-08-21 14:04:12 -0500

    Add accuracy plotting functions

---
+++ plot/plot_acc_table.R
@@ -0,0 +1,74 @@
+.read.process <- function(table){
+    # Read in the table
+    df <- read.table(table,sep=",",header=FALSE)
+
+    # Name the cols
+    colnames(df) <- c("roi", "sub", "set", "acc")
+    print(str(df))
+
+    # Drop overall
+    mask <- df[["set"]] != "overall"
+    df <- df[mask,]
+    df
+}
+
+
+.create.control.factor <- function(df){
+    # Create a label for the control factor,
+    # name it roi_type
+    control_mask <- (df[["roi"]] == "left_ventrical") | (df[["roi"]] == "right_ventrical")
+    control <- rep("exp", nrow(df))
+    control[control_mask] = "control"
+
+    df[["roi_type"]] <- control
+    df
+}
+
+
+plot_acc_table_hist <- function(table, name, height, width){
+    library("ggplot2")
+    
+    df <- .read.process(table) 
+    df <- .create.control.factor(df)
+
+    # Get median of control
+    control_mask <- df[["roi_type"]] == "control"
+    control_median <- median(df[control_mask,"acc"])
+
+    # And plot.
+    pdf(file=name, height=height, width=width)
+
+    p <- ggplot(aes(x=acc, fill=roi_type), data=df)
+    p <- p + facet_grid(roi~.)
+    p <- p + geom_density(aes(alpha=0.5)) + theme_bw()
+    p <- p + geom_vline(xintercept = control_median, colour="grey")
+
+    print(p)
+    dev.off()
+
+}
+
+
+plot_acc_table_box <- function(table, name, height, width){
+    library("ggplot2")
+    
+    df <- .read.process(table)
+    df <- .create.control.factor(df)
+
+    # Get median of control
+    control_mask <- df[["roi_type"]] == "control"
+    control_median <- median(df[control_mask,"acc"])
+
+    # And plot.
+    pdf(file=name, height=height, width=width)
+
+    p <- ggplot(aes(x=roi, y=acc, fill=roi_type), data=df)
+    p <- p + geom_boxplot(notch=TRUE) + theme_bw()
+    p <- p + geom_hline(yintercept = control_median, 
+            colour="grey") + coord_flip()
+
+    print(p)
+    dev.off()
+}
+
+
