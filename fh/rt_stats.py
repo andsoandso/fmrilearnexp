36b2c3fe03d7f68a142d040d03d3cef330b3fe83
Erik Peterson <erik.exists@gmail.com>
2013-08-21 15:00:58 -0400

    metaaccumulate port debug/fixup

--- fh/rt_stats.py
+++ fh/rt_stats.py
@@ -1,11 +1,26 @@
-"""usage: RT_corr roiname savename
+""" See exp() for details:
+
+Usage
+-----
+* Command line:
+    python ./motor_stats.py   ## Runs many ROIs (runtime is ~10 hours)
+
+* ipython:
+    >>> from fmrilearnexp.fh.motor_stats import exp
+    >>> # Use the known good functional ROI....
+    >>> exp("respXtime_rfx_mask",
+            "/data/data2/meta_accumulate/fh/roinii",
+            "/data/data2/meta_accumulate/fh/fidl",
+            "fm_motor_stat_accuracy_test.txt"
+            True)
 """
-import os, sys
+import os
 import numpy as np
 import pandas as pd
 
-from multiprocessing import Pool
+from functools import partial
 
+# sklearn imports.
 from sklearn.preprocessing import scale
 from sklearn.cross_validation import KFold
 from sklearn.ensemble import GradientBoostingClassifier
@@ -13,248 +28,190 @@ from sklearn.svm import SVC
 from sklearn.metrics import (classification_report, accuracy_score,
         precision_score, recall_score, f1_score)
 
-from metaaccumulate.datatools.load import load_roi, create_y
-from metaaccumulate.datatools.process import (remove_invariant_features,
-        smooth)
+# fmrilean imports.
+from fmrilearn.load import load_nii
+from fmrilearn.save import (save_accuracy_table, reset_accuracy_table)
+from fmrilearn.preprocess.data import (remove_invariant_features, 
+        create_X_stats, smooth)
+from fmrilearn.preprocess.labels import (construct_targets, 
+        construct_filter, filter_targets, merge_labels)
+from fmrilearn.info import (print_target_info, print_X_info, print_clf_info,
+        print_label_counts)
+from fmrilearn.classify import simpleCV
 
-# HISTORY
-# * r1
-# * r2 uses CSV C=10, rbf kernel, removed mean and var
-def _save_accuracy_table(name, meta, acc):
-    f = open(name,'a')
+# wheelerdata import
+from wheelerdata.load.fh import (get_roi_data_paths,
+        get_RT_metadata_paths)
+
+
+def exp(roi, table, verbose=True):
+    """
+    An experimental function to classify a ROI from the 
+    face/house data by motor response.
     
-    # Convert meta if it's a seq
-    if isinstance(meta, str):
-        metastr = meta
-    else:
-        metastr = ",".join([str(m) for m in meta])
+    Parameters
+    ----------
+    roi - the name of the roi to use
+    roipath - the full path to the nifti data to classfiy
+    metapath - the full path to the metadata (labels)
+    table - the name of results table.
+    verbose - Print debugging/status info (True).  If False this 
+        function is silent.
     
-    f.write("{0},{1}\n".format(metastr, acc))
-    f.close()
-
-
-def _create_X_stats(X, trial_index, labels):
-    trials = np.unique(trial_index)
-
-    # ----
-    # Init
-    Xmax = np.zeros((trials.shape[0], X.shape[1]))
-    Xmin = np.zeros_like(Xmax)
-    #Xmean = np.zeros_like(Xmax)
-    #Xvar = np.zeros_like(Xmax)
-
-    # ----
-    # Create the stats
-    newlabels = []
-    for ii, trial in enumerate(trials):
-        # Locate this trials data
-        mask = trial == trial_index
-        x_trial = X[mask,:]
-
-        # Get time to peak/min
-        Xmax[ii,:] = np.argmax(x_trial, axis=0)
-        Xmin[ii,:] = np.argmin(x_trial, axis=0)
-        
-        # And their diff
-        Xdiff = Xmax - Xmin
-        
-        # Finally get trial means and variances
-        #Xmean[ii,:] = x_trial.mean()
-        #Xvar[ii,:] = x_trial.var()
-
-        # Only need one label for each trial
-        # now, the first is as good as any
-        newlabels.append(labels[mask][0])
+    Return
+    ------
+    Saves a table of classification accuracies for each subject's 
+        cross validation fold (n=5) as well as the overall subject 
+        mean.
     
-    Xfea = np.hstack([Xmax, Xmin, Xdiff])
+    Data
+    ----
+        * Face/house
+        * Note: runs each subject in the face/house set in a separate
+            classification experiment.
 
-    return Xfea, np.array(newlabels)
+    Details
+    -------
+        * Classes:
+            1. 'rt', i.e. reaction times - 'rt2'/'rt3'
 
-
-# ----
-# OVERVIEW:
-# * Load each Ss data, 
-# * filter by TR, 
-# * filter by resp, 
-# * split into three folds, 
-# * CV train/test manually, 
-# * print the results
-def exp(roi):
-    accuracy_table_name = "rtstats_accuracy_r2.txt"
-
-    roibasepath = "/data/data2/meta_accumulate/fh/roinii"
-    roipaths = [
-        os.path.join(roibasepath, roi+"_9.nii.gz"),
-        os.path.join(roibasepath, roi+"_11.nii.gz"),
-        os.path.join(roibasepath, roi+"_13.nii.gz"),
-        os.path.join(roibasepath, roi+"_14.nii.gz"),
-        os.path.join(roibasepath, roi+"_15.nii.gz"),
-        os.path.join(roibasepath, roi+"_17.nii.gz"),
-        os.path.join(roibasepath, roi+"_19.nii.gz"),
-        os.path.join(roibasepath, roi+"_21.nii.gz"),
-        os.path.join(roibasepath, roi+"_23.nii.gz"),
-        os.path.join(roibasepath, roi+"_24.nii.gz"),
-        os.path.join(roibasepath, roi+"_25.nii.gz"),
-        os.path.join(roibasepath, roi+"_26.nii.gz"),
-        os.path.join(roibasepath, roi+"_27.nii.gz"),
-        os.path.join(roibasepath, roi+"_28.nii.gz")
-        ]
-
-    metabasepath = "/data/data2/meta_accumulate/fh/fidl"
-    metapaths = [
-        os.path.join(metabasepath, "trtime_fh009_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh011_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh013_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh014_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh015_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh017_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh019_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh021_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh023_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh024_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh025_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh026_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh027_RT_corr_EF.csv"),
-        os.path.join(metabasepath, "trtime_fh028_RT_corr_EF.csv")
-    ]
-
+        * Preprocessing:
+            1. remove_invariant_features, 
+            2. smooth, 
+            3. create_X_stats
+    """
+    
+    roipaths = get_roi_data_paths(roi)
+    metapaths = get_RT_metadata_paths()
+    
     # ----
     # Loop overs Ss data, preprocess, and classify
     overall_accuracy = []
     for roipath, metapath in zip(roipaths, metapaths):
-        print("----")
-        print("Loading data.")
-        print("\t{0}".format(roipath))
-        print("\t{0}".format(metapath))
+        if verbose:
+            print("----")
+            print("Loading data.")
+            print("\t{0}".format(roipath))
+            print("\t{0}".format(metapath))
     
         _, roiname = os.path.split(roipath)
-                ## USe roiname in the accuracy tables
+                ## Use roiname in the accuracy tables
 
         # ----
         # Load and process labels
-        meta = pd.read_csv(metapath)    
-        
-        print("Before filteration.")
-        trs = np.array(meta["TR"].tolist())
-        print("\tNumber of TRs: {0}".format(trs.shape))
-
+        meta = pd.read_csv(metapath)
         reaction_times = np.array(meta["rt"].tolist())
-        print("\tNumber of reaction_times: {0}".format(
-                reaction_times.shape))
-
         trial_index = np.array(meta["trialcount"].tolist())
-        print("\tNumber of trial_index: {0}".format(
-                trial_index.shape))
-
-        Xsp = load_roi(roipath, sparse=True)
-        print("\tXsp shape: {0}".format(Xsp.shape))
-
+        trs = np.array(meta["TR"].tolist())
+    
         # ----
-        # Filtration
+        # Construct targets
+        targets = construct_targets(
+                reaction_times=reaction_times, 
+                trial_index=trial_index,
+                trs=trs)
+    
+        # ----
+        # Get the data, intial preprocessing
+        Xsp = load_nii(roipath, sparse=True)
         Xsp = remove_invariant_features(Xsp, sparse=True)
-        Xsp = Xsp[trs,:]
+        Xsp = Xsp[targets["trs"],:]
 
-        rt_only = (reaction_times == "rt2") | (reaction_times == "rt3") 
-        rt_only = np.arange(reaction_times.shape[0])[rt_only]
-            ## Convert bool mask to int for sparse array compatiblity
+        if verbose:
+            print("Before filtration.")
+            print_target_info(targets)
+            print_X_info(Xsp)
+
+        # --
+        # Filter...
+        # --
+        # By rt
+        keepers = ["rt1", "rt2", "rt3", "rt4"]
+        keep_rts = construct_filter(targets["reaction_times"], keepers, True)
+        targets = filter_targets(keep_rts, targets)
+        Xsp = Xsp[keep_rts,:]
         
-        reaction_times = reaction_times[rt_only]
-        trial_index = trial_index[rt_only]
-
-        Xsp = Xsp[rt_only,:]
-
-        print("After filteration.")
-        print("\tNumber of TRs: {0}".format(trs.shape))
-        print("\tNumber of reaction_times: {0}".format(
-                reaction_times.shape))
-        print("\tNumber of trial_index: {0}".format(
-                trial_index.shape))
-        print("\tXsp shape: {0}".format(Xsp.shape))
- 
-        # ----
+        
+        # --
+        # Combine reaction_times into fast and slow
+        fsmap = {
+            "rt1": "slow", "rt2": "slow", 
+            "rt3": "fast", "rt4" : "fast"
+        }
+        targets["fast_slow"] = merge_labels(targets["reaction_times"], fsmap)
+        
+        if verbose:
+            print("After filtration.")
+            print_target_info(targets)
+            print_X_info(Xsp)
+        
+        # --
         # Smooth (and desparse)
         X = smooth(Xsp.todense(), tr=1.5, ub=0.06, lb=0.001)
-
-        # ----
-        # Create stats
-        X, reaction_times = _create_X_stats(X, trial_index, reaction_times)
-
-        print("After smooth and stat creation.")
-        print("\tNumber of trials: {0}".format(
-                np.unique(trial_index).shape))
-        print("\tNumber of reaction_times: {0}".format(
-                reaction_times.shape))
-        print("\tX shape: {0}".format(X.shape))
         
-        print("Label counts:")
-        for rt in np.unique(reaction_times):
-            print("\t({0}, {1})".format(rt, np.sum(reaction_times == rt)))
-
+        # --
+        # Create stats
+        Xstat, fast_slow_stat = create_X_stats(
+                X, 
+                targets["trial_index"], 
+                targets["fast_slow"])
+        
         # ----
-        # Create train/class paritions
-        nrow, ncol = X.shape
+        # --
+        # CV setup
+        nrow, ncol = Xstat.shape
         cv = KFold(nrow, n_folds=5, indices=True)
         
-        # ----
-        # Loop over the folds, class as we go
-        print("Classifying.")
-        i = 0
-        y = create_y(reaction_times)
-        for train_index, test_index in cv:
-            # ----
-            # Partition the data
-            Xtrain = X[train_index,:]
-            Xtest = X[test_index,:]
-            print("\tShapes of Xtrain and Xtest: {0}, {1}".format(
-                    Xtrain.shape, Xtest.shape))
-            
-            ytrain = y[train_index]
-            ytest = y[test_index]
-            print("\tNumber of ytrain and ytest: {0}, {1}".format(
-                    ytrain.shape, ytest.shape))
+        # --
+        # clf setup
+        clf = GradientBoostingClassifier(
+                n_estimators=100, learning_rate=1.0, 
+                max_depth=1, random_state=0)
+        
+        # --
+        if verbose:
+            print("Classifying...")
+            print_label_counts(fast_slow_stat)
+            print_clf_info(clf)
 
-            # ----
-            # Init clf
-            #clf = GradientBoostingClassifier(
-            #        n_estimators=100, learning_rate=1.0, 
-            #        max_depth=1, random_state=0)
-            clf = SVC(C=10, kernel='rbf')
-            # ----
-            # Class!
-            clf.fit(scale(Xtrain), ytrain)
-            predictions = clf.predict(scale(Xtest))
-            
-            # ----
-            # And print the results
-            accuracy = accuracy_score(ytest, predictions) 
-            print("\tFor CV {0}, accuracy was {1}".format(
-                    i, accuracy))        
-            
-            # Store the accuracy
+        truths, predictions = simpleCV(Xstat, fast_slow_stat, cv, clf, verbose)
+        
+        # ----
+        # Save the results
+        i = 0
+        for truth, prediction in zip(truths, predictions):
+            accuracy = accuracy_score(truth, prediction)
             overall_accuracy.append(accuracy)
             
-            print("\tSaving accuracy")
-            _save_accuracy_table(
-                accuracy_table_name, 
+            save_accuracy_table(
+                table, 
                 [roi, roiname, "cv_"+str(i)], 
                 accuracy)
-            
-            # ----
-            # Update the Cv counter
-            i += 1
+        
+            if verbose:
+                print("Processing the CV results...")
+                print("\tFor CV {0}, accuracy was {1}".format(i, accuracy))
+                print("\tFull report:")
+                print(classification_report(truth, prediction, 
+                        target_names=sorted(np.unique(fast_slow_stat))))
+                i += 1
     
-    print("****")
     omean = np.array(overall_accuracy).mean()
-    print("Overall accuracy for {1} was {0}".format(omean, roi))
+    save_accuracy_table(table, [roi, roi, "overall"], omean)
+    
+    if verbose:
+        print("****")
+        print("Overall accuracy for {1} was {0}".format(omean, roi))
 
-    print("Saving accuracy")
-    _save_accuracy_table(accuracy_table_name, 
-            [roi, roi, "overall"], 
-            omean)
-
-
+        
 if __name__ == "__main__":
-    rois = ["respXtime_rfx_mask",
+    # ----
+    # Experimental init
+    
+    # --
+    # Exp globals
+    ACCURACY_TABLE_NAME = "fh_rt_stats_accuracy.txt"
+    ROIS = ["respXtime_rfx_mask",
             "left_ventrical",
             "right_ventrical",
             "left_putamen",
@@ -282,6 +239,18 @@ if __name__ == "__main__":
             "right_hippocampus",
             "parahippo_anterior",
             "parahippo_posterior"]
-
-    [exp(roi) for roi in rois]
+    
+    # --     
+    # Pre-run cleanup
+    reset_accuracy_table(ACCURACY_TABLE_NAME)
+       
+    # --
+    # A little PFA to simplify the run loop...
+    named_exp = partial(exp,  
+            table=ACCURACY_TABLE_NAME,
+            verbose=True)
+    
+    # ----
+    # And go!
+    [named_exp(roi) for roi in ROIS]
 
