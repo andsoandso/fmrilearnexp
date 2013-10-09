bbbc86607c7a19e6cf5a53794003ca32b33fccd0
Erik Peterson <erik.exists@gmail.com>
2013-10-09 16:57:54 -0400

    New exps.

---
+++ fh/kpca_time.py
@@ -0,0 +1,47 @@
+"""PCA reduction for all voxels and trials.  Results saved to a series
+of tables named like <name>_<roi>_<cond>.csv.
+
+usage: python ./kpca_time.py name roifile cond [, filtfile]
+"""
+
+import sys, os
+import numpy as np
+
+from sklearn.decomposition import KernelPCA
+
+from fmrilearn.load import load_roifile
+from fmrilearnexp.base import Time
+from fmrilearnexp.base import DecomposeFH
+
+
+# ---------------------------------------------------------------------------
+# Process argv
+# ---------------------------------------------------------------------------
+# pos: (1) name (2) roifile (3) cond
+# op: (4) filtfile
+if len(sys.argv) == 4:
+    basename = sys.argv[1]
+    _, rois = load_roifile(sys.argv[2])
+    cond = sys.argv[3]
+    filtfile = None
+elif len(sys.argv) == 4:
+    basename = sys.argv[1]
+    _, rois = load_roifile(sys.argv[2])
+    cond = sys.argv[3]
+    filtfile = sys.argv[4]
+else:
+    raise ValueError("Wrong number of arguments")
+
+# ---------------------------------------------------------------------------
+# Setup exp
+# ---------------------------------------------------------------------------
+kernel = "rbf"
+spacetime = Time(KernelPCA(6, kernel=kernel))
+exp = DecomposeFH(spacetime, window=11, nsig=3)
+
+# ---------------------------------------------------------------------------
+# And run each roi
+# ---------------------------------------------------------------------------
+for n, roi in enumerate(rois):
+    print("{0} ({1}/{2})".format(roi, n+1, len(rois)))   ## Progress marker
+    exp.run(basename, roi, cond, smooth=False, filtfile=None)
