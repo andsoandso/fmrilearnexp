86d5329ec33a89884b7049455dbde4e25f6ca60b
Erik Peterson <erik.exists@gmail.com>
2013-11-22 14:10:59 -0500

    meta versions of this experiments ready for use

--- meta/fir.py
+++ meta/fir.py
@@ -1,18 +1,16 @@
-"""EVA reduction for all voxels and trials.  Results saved to a series
-of tables named like <name>_<roi>_<cond>.csv.
-
-usage: python ./eva.py name data roifile cond [, filtfile]
+"""
+usage: python ./fir.py name data roifile cond [, filtfile]
 """
 import sys, os
 import numpy as np
 
-from fmrilearn.load import load_roifile
 from fmrilearn.analysis import fir
 
 from fmrilearnexp.base import AverageTime
 from fmrilearnexp.base import DecomposeExp
-from wheelerdata.load.meta import get_data
 from fmrilearnexp.common import process_exp_argv
+
+from wheelerdata.load.meta import get_data
 
 
 # ---------------------------------------------------------------------------
@@ -31,7 +29,7 @@ exp = DecomposeExp(spacetime, data, window=15, nsig=3)
 # And run each roi
 # ---------------------------------------------------------------------------
 for n, roi in enumerate(rois):
-    print("{0} ({1}/{2})".format(roi, n+1, len(rois)))   ## Progress marker
+    print("{3}: {0} ({1}/{2})".format(roi, n+1, len(rois), dataname))   
     exp.run(basename, roi, cond, smooth=False, filtfile=filtfile, event=True)
 
 
--- meta/pca_space.py
+++ meta/pca_space.py
@@ -1,61 +1,25 @@
-"""PCA reduction for all voxels and trials.  Results saved to a series
-of tables named like <name>_<roi>_<cond>.csv.
-
+"""
 usage: python ./pca_space.py name data roifile cond [, filtfile]
 """
 
 import sys, os
 import numpy as np
 
-from sklearn.decomposition import PCA
-
-from fmrilearn.load import load_roifile
 from fmrilearn.analysis import fir
+
+from sklearn.decomposition import PCA
 
 from fmrilearnexp.base import Space
 from fmrilearnexp.base import DecomposeExp
+from fmrilearnexp.common import process_exp_argv
 
-from wheelerdata.load.butterfly import Butterfly
-from wheelerdata.load.clock import Clock
-from wheelerdata.load.fh import FH
-from wheelerdata.load.polygon import Polygon
-from wheelerdata.load.redgreen import Redgreen
-
+from wheelerdata.load.meta import get_data
 
 # ---------------------------------------------------------------------------
 # Process argv
 # ---------------------------------------------------------------------------
-if len(sys.argv) == 4:
-    basename = sys.argv[1]  ## name
-    dataname = sys.argv[2]  ## data
-    _, rois = load_roifile(sys.argv[3])  ## roifile
-    cond = sys.argv[4]  ## cond
-    filtfile = None
-elif len(sys.argv) == 6:
-    basename = sys.argv[1]
-    dataname = sys.argv[2]
-    _, rois = load_roifile(sys.argv[3])
-    cond = sys.argv[4]
-    filtfile = sys.argv[5]
-else:
-    raise ValueError("Wrong number of arguments")
-
-# ---------------------------------------------------------------------------
-# Process exp
-# ---------------------------------------------------------------------------
-if dataname == 'fh':
-    data = FH()
-elif dataname == 'butterfly':
-    data = Butterfly()
-elif dataname == 'clock':
-    data = Clock()
-elif dataname == 'polygon':
-    data = Polygon()
-elif dataname == 'redgreen':
-    data = Redgreen()
-else:
-    raise ValueError('Data not understood.  Try fh, butterfly, clock, 
-            polygon, or redgreen')
+basename, dataname, rois, cond, filtfile = process_exp_argv(sys.argv)
+data = get_data(dataname)
 
 # ---------------------------------------------------------------------------
 # Setup exp
@@ -67,6 +31,6 @@ exp = DecomposeExp(spacetime, data, window=15, nsig=3)
 # And run each roi
 # ---------------------------------------------------------------------------
 for n, roi in enumerate(rois):
-    print("{0} ({1}/{2})".format(roi, n+1, len(rois)))
+    print("{3}: {0} ({1}/{2})".format(roi, n+1, len(rois), dataname))   
     exp.run(basename, roi, cond, smooth=False, filtfile=filtfile, event=True)
 
