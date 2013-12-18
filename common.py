7b210333eaebc4f48833cc3cceba213f7c1fffdf
Erik Peterson <erik.exists@gmail.com>
2013-12-18 11:21:00 -0500

    Added private function for classifier data preprocsseing.  It's generality is very, err, small.

--- common.py
+++ common.py
@@ -1,12 +1,15 @@
 """Misclanous utlity functions for these experiments."""
 import os
 import numpy as np
+import pandas as pd
 
+from copy import deepcopy
+
+from fmrilearn.preprocess.labels import create_y
 from fmrilearn.load import load_roifile
 from fmrilearn.preprocess.data import checkX
 from fmrilearn.preprocess.split import by_labels
 from fmrilearn.preprocess.reshape import by_trial
-
 
 
 def process_exp_argv(argv):
@@ -52,3 +55,87 @@ def join_by_underscore(append_csv, *args):
 def sum_explained_variance(pcar):
     return np.round(np.sum(pcar.explained_variance_ratio_), decimals=2)
 
+
+def _create_X_y_xcs(csvs, data, labels_j, trial_tr_j):
+    # Check data exists
+    feature_index = range(*[int(i) for i in data.split(':')])
+    for csvn in csvs:
+        if not os.path.exists(csvn):
+            raise IOError("{0} does not exist".format(csvn))
+
+    X, y, index, tTR = None, None, None, None
+    for csvn in csvs:
+        dftmp = pd.read_csv(csvn, sep=",", header=0)
+
+        ## Reinit 
+        if (X is None) and (y is None):
+            X = np.asarray(dftmp.ix[:,feature_index])
+            y = np.asarray(dftmp.ix[:,labels_j], dtype=np.str)
+            tTR = np.asarray(dftmp.ix[:,trial_tr_j])
+        ## Otherwise stack
+        else:
+            X = np.vstack([X, np.asarray(dftmp.ix[:,feature_index])])
+            y = np.concatenate([y, np.asarray(dftmp.ix[:,labels_j], 
+                    dtype=np.str)])
+            tTR = np.concatenate([tTR, np.asarray(dftmp.ix[:,trial_tr_j])])
+
+    # Sane so far?
+    assert checkX(X)
+    assert X.shape[0] == y.shape[0], "X and y length mismatch"
+
+    # ----
+    # Preproces
+    # ----
+    # Convert y to integer codes
+    y = create_y(y)
+
+    # Convert trial_tr to a trial_index fot stat gen
+    cnt = 0
+    last_ttr = 0
+    trial_index = []
+    for ttr in tTR:
+        ## If the TR is not monotonic, new trial
+        if ttr < last_ttr:  
+            cnt += 1
+        trial_index.append(cnt)
+        last_ttr = deepcopy(ttr)
+
+    # ----
+    # Convert X to Xstat
+    # --
+    # Init
+    trials = np.unique(trial_index)
+    Xmax = np.zeros((trials.shape[0], X.shape[1]))
+    Xmin = np.zeros_like(Xmax)
+    Xmean = np.zeros_like(Xmax)
+    Xvar = np.zeros_like(Xmax)
+    ystat = []
+    indexstat = []
+    for ii, trial in enumerate(trials):
+        # Locate this trials data
+        # Get time to peak/min
+        # and their diff
+        # also get trial means and variances
+        # finally reorg metadata.
+        mask = trial == trial_index
+        x_trial = X[mask,:]
+
+        Xmax[ii,:] = np.argmax(x_trial, axis=0)
+        Xmin[ii,:] = np.argmin(x_trial, axis=0)
+        Xdiff = Xmax - Xmin
+        Xmean[ii,:] = x_trial.mean()
+        Xvar[ii,:] = x_trial.var()
+
+        ystat.append(y[mask][0])
+
+    # And rename
+    X = np.hstack([Xmax, Xmin, Xdiff, Xmean, Xvar])
+    y = np.asarray(ystat)
+    index = np.asarray(indexstat)
+    del Xmax, Xmin, Xdiff, Xmean, Xvar ## Cleanup ASAP
+
+    # Still sane?
+    assert checkX(X)
+    assert X.shape[0] == y.shape[0], "X and y length mismatch"
+
+    return X, y
