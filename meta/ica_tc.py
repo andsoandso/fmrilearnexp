662588cdd3cb95836ab8015c78db88a522d08397
type <type@simon.local>
2014-05-27 11:40:19 -0700

    Fixed imports

--- meta/ica_tc.py
+++ meta/ica_tc.py
@@ -8,9 +8,6 @@ import argparse
 
 # from fmrilearn.analysis import fir
 from fmrilearn.load import load_roifile
-from sklearn.decomposition import FastICA
-from fmrilearnexp.base import Timecourse
-from fmrilearnexp.base import DecomposeExp
 from wheelerdata.load.meta import get_data
 
 parser = argparse.ArgumentParser(
